106f9b4f92ca43661615cab753b35e702f4487db
wondieki <wkerubo28@gmail.com>
2025-02-08 17:26:04 +0300

    Done

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None  # Use a private attribute to enforce integer validation
+        self.size = size  # Call the setter to validate input
+        self.condition = "New"  # Default condition
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
